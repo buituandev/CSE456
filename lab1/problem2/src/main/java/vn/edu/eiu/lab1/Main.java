562dca129f16eddc6444ee0c73ed42ee75339e64
Bui Anh Tuan <147406431+buituandev@users.noreply.github.com>
2025-07-03 10:44:11 +0700

    Implement JDBC connection and data retrieval from MySQL database

--- lab1/problem2/src/main/java/vn/edu/eiu/lab1/Main.java
+++ lab1/problem2/src/main/java/vn/edu/eiu/lab1/Main.java
@@ -1,17 +1,44 @@
 package vn.edu.eiu.lab1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Connection conn;
+        //Sử dụng các class cung cấp sẵn có trong thư viên JDBC (có trong JAVA SDK)
+        //JDBC sẽ tự động kết nối với MySQL JDBC Server (MySQL connector/java)
+        try {
+            String url = "jdbc:mysql://localhost:3306/cse456_q4_2025";
+            var user = "root";
+            var password = "tuansql2003";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            //Đối với Java mới thì Driver sẽ được tự động dò tìm trong URL mà ko cần lệnh này.
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to database successfully");
+
+            PreparedStatement pstmt = conn.prepareStatement("select * from Student");
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                String id = rs.getString(1);
+                String firstName = rs.getString("FirstName");
+                String lastName = rs.getString("LastName");
+                int yearOfBirth = rs.getInt("yob");
+                double gpa = rs.getDouble("gpa");
+//                System.out.println(id + "|" + firstName + "|" + lastName + "|" + yearOfBirth + "|" + gpa);
+//                System.out.printf("\033[32m%s\033[0m|\033[34m%s\033[0m|\033[35m%s\033[0m|\033[33m%d\033[0m|\033[36m%.2f\033[0m%n", id, firstName, lastName, yearOfBirth, gpa);
+                System.out.printf("|%4s|%-10s|%-10s|%4d|%4.2f|%n", id, firstName, lastName, yearOfBirth, gpa);
+            }
+
+
+            conn.close();
+            System.out.print("Connection closed");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
